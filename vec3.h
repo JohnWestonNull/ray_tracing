2c599aa5c49f001e1fcd8377bd274be916948588
JohnWestonNull <799433746@qq.com>
2021-04-20 22:48:02 +0800

    complete

--- vec3.h
+++ vec3.h
@@ -125,11 +125,11 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
-inline vec3 reflect(const vec3& v, const vec3& n) {
-    return v - 2*dot(v,n)*n;
+inline vec3 reflect(const vec3 &v, const vec3 &n) {
+    return v - 2 * dot(v, n) * n;
 }
 
-inline vec3 refract(const vec3& v, const vec3& n, double etai_over_etat) {
+inline vec3 refract(const vec3 &v, const vec3 &n, double etai_over_etat) {
     auto cos_theta = fmin(dot(-v, n), 1.0);
     vec3 r_out_perp = etai_over_etat * (v + cos_theta * n);
     vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
@@ -139,6 +139,14 @@ inline vec3 refract(const vec3& v, const vec3& n, double etai_over_etat) {
 vec3 random_in_unit_sphere() {
     while (true) {
         auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
         if (p.length_squared() >= 1) continue;
         return p;
     }
